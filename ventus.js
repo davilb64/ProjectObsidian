01c0af64ad9f47c265738b23fbf6eaa53db969d0
Davi Lopes Brito <davilopesbrito64@gmail.com>
2025-02-13 21:42:34 -0300

    add até ventus

--- ventus.js
+++ ventus.js
@@ -1,8 +1,8 @@
 function verificarResposta() {
     let resposta = document.getElementById("resposta").value;
     
-    if (resposta === "morbi") {  
-        document.getElementById("resultado").innerHTML = "✅ Correto! <a href='morbi.html'>Próximo desafio</a>";
+    if (resposta === "ventus") {  
+        document.getElementById("resultado").innerHTML = "✅ Correto! <a href='ventus.html'>Próximo desafio</a>";
     } else {
         document.getElementById("resultado").innerHTML = "❌ Errado! Tente novamente.";
     }
