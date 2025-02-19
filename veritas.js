66b8f6a4700e6aebd22fff604c8ff5b61ad53b92
Davi Lopes Brito <davilopesbrito64@gmail.com>
2025-02-19 09:11:57 -0300

    att penultima

--- veritas.js
+++ veritas.js
@@ -1,8 +1,8 @@
 function verificarResposta() {
     let resposta = document.getElementById("resposta").value;
     
-    if (resposta === "originis") {  
-        document.getElementById("resultado").innerHTML = "✅ Correto! <a href='originis.html'>Próximo desafio</a>";
+    if (resposta === "veritas") {  
+        document.getElementById("resultado").innerHTML = "✅ Correto! <a href='veritas.html'>Próximo desafio</a>";
     } else {
         document.getElementById("resultado").innerHTML = "❌ Errado! Tente novamente.";
     }
